02ca53847a093c86e278ced114b489a6aeb25dda
KsTech95 <133238464+junioubm65@users.noreply.github.com>
2023-05-11 21:35:02 +0700

    Add all file of Project
    NodeJS Express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+app.get("/",(req,res) => {
+    res.send('Hello June');
+});
+
+app.listen(port,() => {
+    console.log("Listening on port : " + chalk.green(port));
+    debug("Listening on port : " + chalk.green(port));
+});
